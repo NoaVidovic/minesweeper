59dc86d06c9190bb40de22e0c2597347e5d3ed75
Noa Vidovic <noavidovic.yup@gmail.com>
2019-09-21 02:48:41 +0200

    + added starting code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Minefield extends React.Component {
+	constructor(props) {
+		super(props);
+
+		let array = Array(9);
+		for (let i = 0; i < 9; i++)
+			array[i] = i;
+
+		this.state = {
+			squares: array
+		};
+	}
+
+	renderSquare(i) {
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => alert(i)}
+			/>
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+class Screen extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Minefield />
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Screen />,
+	document.getElementById('root')
+);
