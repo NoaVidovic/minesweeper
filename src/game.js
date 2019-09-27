c24b91790cd58d67a2ebcdf5793f85a58f1b4bef
Noa Vidovic <noavidovic.yup@gmail.com>
2019-09-27 14:56:27 +0200

    + added find mine functionality

--- src/game.js
+++ src/game.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Square(props) {
     return (
         <button
-            className="square"
+            className="Square"
             style={{height: props.size, width: props.size}}
             onClick={props.onClick}>
-            {props.value}
+            {props.state}
         </button>
     )
 }
@@ -16,47 +16,99 @@ class Minefield extends React.Component {
         super(props);
 
         //populates an array with mines
-        let mines = Array(props.row_number * props.column_number);
-        for (let i = 0; i < mines.length; i++)
-            mines[i] = i < props.mine_number;
+        let mine_list = Array(props.row_number * props.column_number);
+        for (let i = 0; i < mine_list.length; i++)
+            mine_list[i] =
+                i < props.mine_number
+                    ? 'mine'
+                    : 'idle';
 
         //shuffles mines
-        for (let i = mines.length - 1; i > 0; i--) {
+        for (let i = mine_list.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [mines[i], mines[j]] = [mines[j], mines[i]];
+            [mine_list[i], mine_list[j]] = [mine_list[j], mine_list[i]];
         }
 
         //turns the mine array into a grid
-        let squares = [];
+        let mines = [];
         let num = 0;
         for (let i = 0; i < props.row_number; i++) {
-            squares.push([]);
+            mines.push([]);
 
             for (let j = 0; j < props.column_number; j++)
-                squares[i].push(mines[num++]);
+                mines[i].push(mine_list[num++]);
         }
 
         this.state = {
-            squares: squares
+            mines: mines
         };
     }
 
-    updateSquare() {
-        alert('Square cleared');
+    findMineNum(i, j) {
+        let num = 0;
+        for (let a = i-1; a <= i+1; a++) {
+            if (a < 0 || a >= this.props.row_number)
+                continue;
+
+            for (let b = j-1; b <= j+1; b++) {
+                if (b < 0 && b >= this.props.column_number)
+                    continue;
+
+                if (this.state.mines[a][b] === 'mine')
+                    num++;
+            }
+        }
+        return num;
+    }
+
+    clearAroundZero(i, j) {
+        for (let a = i-1; a <= i+1; a++) {
+            if (a < 0 || a >= this.props.row_number)
+                continue;
+
+            for (let b = j-1; b <= j+1; b++) {
+                if (b < 0 || b >= this.props.column_number)
+                    continue;
+
+                if (this.state.mines[a][b] === 'idle')
+                        this.updateSquare(a, b);
+            }
+        }
+    }
+
+    updateSquare(i, j) {
+        //TODO: change css for clicked square state
+
+        let mines = this.state.mines;
+        const mine_num = this.findMineNum(i, j);
+
+        if (mine_num === 0) {
+            mines[i][j] = '.';
+            this.clearAroundZero(i, j);
+        } else {
+            mines[i][j] = mine_num;
+        }
+
+        this.setState({mines});
     }
 
     handleClick = (i, j) => {
-        if (this.state.squares[i][j])
+        if (this.state.mines[i][j] === 'mine')
             alert('You lost!');
         else
-            this.updateSquare();
+            this.updateSquare(i, j);
     };
 
     renderSquare(i, j, size) {
+        const square = this.state.mines[i][j];
+
         return (
             <Square
                 key={i + ': ' + j}
-                value={this.state.squares[i][j]}
+                state={
+                    ['mine', 'idle'].includes(square)
+                        ? null : square
+                }
                 onClick={() => this.handleClick(i, j)}
                 size={size}
             />
@@ -70,10 +122,10 @@ class Minefield extends React.Component {
 
         const grid = [];
 
-        for (let i = 0; i < this.state.squares.length; i++) {
+        for (let i = 0; i < this.state.mines.length; i++) {
             const grid_row = [];
 
-            for (let j = 0; j < this.state.squares[i].length; j++)
+            for (let j = 0; j < this.state.mines[i].length; j++)
                 grid_row.push(this.renderSquare(i, j, size));
 
             grid.push(
